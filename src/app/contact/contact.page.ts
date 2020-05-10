8dedc8e39cc176fa39e611c32edb1300415eba59
casa <micorreo@example.com>
2020-05-10 20:31:04 +0200

    Add Video Camera

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -3,6 +3,7 @@ import { Contact } from '../models/contact';
 import { ContactsService } from '../services/contacts.service';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-contact',
@@ -13,8 +14,14 @@ export class ContactPage implements OnInit {
   private title: string;
   private mode: string;
   private contact: Contact;
+  private options: CameraOptions = {
+    quality: 100,
+    destinationType: this.camera.DestinationType.DATA_URL,
+    encodingType: this.camera.EncodingType.JPEG,
+    mediaType: this.camera.MediaType.PICTURE
+    };
 
-  constructor(private contacts: ContactsService, private route: ActivatedRoute, private location: Location) {
+  constructor(private camera: Camera, private contacts: ContactsService, private route: ActivatedRoute, private location: Location) {
     if (this.route.snapshot.paramMap.get('id')) {
       this.contact = this.contacts.listContactById(Number(this.route.snapshot.paramMap.get('id')));
       this.mode = this.route.snapshot.queryParamMap.get('mode') || 'view';
@@ -50,5 +57,16 @@ export class ContactPage implements OnInit {
     }
     this.location.back();
   }
+
+  changeAvatar() {
+    console.log('[ContactPage] changeAvatar()');
+    this.camera.getPicture(this.options).then((imageData) => {
+    let base64Image = 'data:image/jpeg;base64,' + imageData;
+    this.contact.avatar = base64Image;
+    }, (err) => {
+    // Handle error
+    }
+    );
+    }
 
   }
